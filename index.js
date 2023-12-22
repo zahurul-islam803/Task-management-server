6dbfce88fe4b1a5fa9641036b92ef4168ff9f753
Zahurul Islam <zahurulislam803@gmail.com>
2023-12-22 15:36:48 +0600

    display todo list

--- index.js
+++ index.js
@@ -47,11 +47,13 @@ async function run() {
       res.send(result);
     })
 
-    // get all users
-    app.get("/users", async (req, res) => {
-      const result = await usersCollection.find().toArray();
+    // get task data by email
+    app.get('/task/:email', async (req, res)=>{
+      const email = req.params.email;
+      const result = await tasksCollection.find({email}).toArray();
       res.send(result);
-    });
+    })
+   
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
