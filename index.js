cc76721968e45ce6ed55349a1bb9cecb42f16e52
Zahurul Islam <zahurulislam803@gmail.com>
2023-12-22 07:37:39 +0600

    setUp server and save user in database

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+// middleware
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const client = new MongoClient(process.env.DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    const usersCollection = client.db("TaskUnity").collection("users");
+
+    // Save or modify user email, status in Database
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const options = { upsert: true };
+      const result = await usersCollection.updateOne(
+        query,
+            {
+              $set: user,
+            },
+            options
+            );
+      res.send(result);
+    });
+
+    // get all users
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from TaskUnity Server..");
+});
+
+app.listen(port, () => {
+  console.log(`TaskUnity is running on port ${port}`);
+});
+
